a485e369f24d213e7ff4d1d543c8edf543d7c9a8
akzi <382018309@qq.com>
2016-11-13 13:40:25 +0100

    add codes

--- include/function_traits.hpp
+++ include/function_traits.hpp
@@ -1,18 +1,27 @@
 #pragma once
 namespace xutil
 {
-	template<typename T>
+	template<typename>
 	struct function_traits;
-	
+
 	template<typename Ret, typename ...Args>
 	struct function_traits<Ret(Args...)>
 	{
-		typedef Ret return_type;
+		typedef std::function<Ret(Args...)> stl_function_type;
 	};
 
 	template<typename Ret, typename ...Args>
-	struct function_traits<Ret(*)(Args...)> 
-	{
-		typedef Ret return_type;
-	};
+	struct function_traits<Ret(*)(Args...)> :function_traits<Ret(Args...)> { };
+
+	template<typename Ret, typename Class, typename ...Args>
+	struct function_traits<Ret(Class::*)(Args...) const> :function_traits<Ret(Args...)> { };
+
+	template<typename Ret, typename Class, typename ...Args>
+	struct function_traits<Ret(Class::*)(Args...)> :function_traits<Ret(Args...)> { };
+
+	template<typename Callable>
+	struct function_traits :function_traits<decltype(&Callable::operator())> { };
+
+	template<typename Ret, typename ...Args>
+	struct function_traits<std::function<Ret(Args...)>> : function_traits<Ret(Args...)> { };
 }
