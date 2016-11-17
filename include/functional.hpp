c5fb1b8a5011f300410cefc0b417996d22fe3511
fuwangqin <382018309@qq.com>
2016-11-17 17:43:14 +0800

    add codes

---
+++ include/functional.hpp
@@ -0,0 +1,30 @@
+#pragma once
+#include <string.h>
+namespace xutil
+{
+	namespace functional
+	{
+		struct strcasecmper
+		{
+			bool operator()(const char *left, const char *right)
+			{
+#ifdef _WIN32
+				return _strcmpi(left, right) == 0;
+#else
+				return strcasecmp(left, right) == 0;
+#endif
+			}
+		};
+		struct strncasecmper
+		{
+			bool operator()(const char *left, const char *right, std::size_t len)
+			{
+#ifdef _WIN32
+				return _strnicmp(left, right, len) == 0;
+#else
+				return strncasecmp(left, right, len) == 0;
+#endif
+			}
+		};
+	}
+}
