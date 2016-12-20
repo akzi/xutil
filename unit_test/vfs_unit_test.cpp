a0d172900ecaa84432e55371eb68b6c29ba985d1
akzi <382018309@qq.com>
2016-12-20 11:51:04 +0800

    add codes

--- unit_test/vfs_unit_test.cpp
+++ unit_test/vfs_unit_test.cpp
@@ -1,19 +1,18 @@
-#include "../../../xtest/include/xtest.hpp"
+#include "../../xtest/include/xtest.hpp"
 #include "../include/vfs.hpp"
 xtest_run;
 
-using namespace xutil;
 
 XTEST_SUITE(vfs)
 {
 	XUNIT_TEST(file_type)
 	{
-		auto type = vfs::file_type()("F:\\vpn.txt");
+		auto type = xutil::vfs::file_type()("F:\\vpn.txt");
 	}
 	XUNIT_TEST(ls_files)
 	{
-		auto current_dir = vfs::getcwd()();
-		auto files = xutil::vfs::ls_files()(current_dir ,3);
+		auto current_dir = xutil::vfs::getcwd()();
+		auto files = xutil::vfs::ls_files()("/home/skyinno/akzi/" ,3);
 
 		for (auto &itr : files)
 			std::cout << itr << std::endl;
